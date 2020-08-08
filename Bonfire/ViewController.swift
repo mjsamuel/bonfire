e9cbd190fe50bf476b5264b15ce9dbb725573896
mjsamuel <matthew.samuel@outlook.com.au>
2020-08-08 13:13:20 +1000

    Initial Xcode commit

---
+++ Bonfire/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Bonfire
+//
+//  Created by Matthew Samuel on 8/8/20.
+//  Copyright © 2020 ipse. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
