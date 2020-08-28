abb0b9099b464cd58b73147d185dd433a4ee9421
rmit-s3718036-dylan-sbogar <dylansbogar@gmail.com>
2020-08-29 01:21:46 +1000

    Initial Commit
    Created the table view based off the current implementation on the requests storyboard. Also implemented an unfinished detail view for adding new DNS listings with basic constraints. Also created controller and viewModel.swift files but yet to implement them into anything. Intend on finishing some form of a layout on the UI for creating new DNS listings and editing existing ones before skipping ahead to the api implementation.

---
+++ Bonfire/Controller/DNSController.swift
@@ -0,0 +1,15 @@
+//
+//  DNSController.swift
+//  Bonfire
+//
+//  Created by Dylan Sbogar on 29/8/20.
+//  Copyright © 2020 ipse. All rights reserved.
+//
+
+import UIKit
+
+class DNSController: UITableViewController {
+    
+    
+}
+
