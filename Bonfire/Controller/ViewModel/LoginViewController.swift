1c863c83b335b385b189c6bbb72e25852f3bcb4a
Kurrt <dev@kurrt.com>
2020-08-28 18:53:38 +1000

    Updated to new design. Kurt:1 Autolayout: 0.

---
+++ Bonfire/Controller/ViewModel/LoginViewController.swift
@@ -0,0 +1,29 @@
+//
+//  LoginViewController.swift
+//  Bonfire
+//
+//  Created by Kurt Invernon on 28/8/20.
+//  Copyright © 2020 ipse. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
