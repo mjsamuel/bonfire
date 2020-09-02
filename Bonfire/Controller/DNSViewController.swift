e91233fe8eedd68e94993ce4d30d60594a9f3ebe
rmit-s3718036-dylan-sbogar <dylansbogar@gmail.com>
2020-09-02 21:55:01 +1000

    Display TableView Cells
    the table view controller can now display cells (unconfigured ones using the default template given) up to any certain given amount. Next step is to figure out how to grab the DNS records from the api data and display accordingly for multiple cells

---
+++ Bonfire/Controller/DNSViewController.swift
@@ -0,0 +1,35 @@
+//
+//  DNSViewController.swift
+//  Bonfire
+//
+//  Created by Dylan Sbogar on 29/8/20.
+//  Copyright © 2020 ipse. All rights reserved.
+//
+
+import UIKit
+
+class DNSViewController: UITableViewController {
+    private let viewModel = DNSViewModel()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+        // return DNS.count?
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "dnsIdentifier", for: indexPath)
+        
+        // configure cell
+        
+        return cell
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        
+    }
+}
+
--- Bonfire/Controller/ViewModel/DNSViewModel.swift
+++ Bonfire/Controller/ViewModel/DNSViewModel.swift
@@ -9,5 +9,26 @@
 import Foundation
 
 struct DNSViewModel {
+    private let bonfire: Bonfire = Bonfire.sharedInstance
+    private var DNSdata: [[String: Any]] = []
     
+    init() {
+        updateData()
+    }
+    
+    /**
+     Pulls data from the Cloudflare API
+     */
+    public mutating func updateData() {
+        let zoneId = bonfire.currentZone!.getId()
+        if let rawDNS = bonfire.cloudflare!.getRequests(zoneId: zoneId) {
+            self.DNSdata = rawDNS
+        }
+    }
+}
+
+struct DNS {
+    let name: String
+    let content: String
+    let type: String
 }
