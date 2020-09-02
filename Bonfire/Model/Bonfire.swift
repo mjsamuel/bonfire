8f8a02c22ab808062ecd669f2c1c13ff8251bf5a
Kurrt <dev@kurrt.com>
2020-09-02 18:11:56 +1000

    Completed the login flow

---
+++ Bonfire/Model/Bonfire.swift
@@ -0,0 +1,48 @@
+//
+//  Bonfire.swift
+//  Bonfire
+//
+//  Copyright © 2020 ipse. All rights reserved.
+//
+import Foundation
+
+class Bonfire {
+    static let sharedInstance = Bonfire()
+    
+    public var cloudflare: Cloudflare?
+    public var zones: [Zone]?
+    public var currentZone: Zone?
+    
+    private init() {
+        cloudflare = Cloudflare(email: "...", apiKey: "...")
+        zones = cloudflare?.getZones()
+        currentZone = zones?[0]
+    }
+    
+    /**
+     Logs in a user with thier credentials
+     
+     - Parameters:
+     - email: The email of the user
+     - apiKey: The api key corresponding to that account
+     */
+    public func login(email: String, apiKey: String) {
+        cloudflare = Cloudflare(email: email, apiKey: apiKey)
+        zones = cloudflare?.getZones()
+        currentZone = zones?[0]
+        if (cloudflare != nil) {
+            cloudflare!.isLoggedIn = true
+        }
+    }
+    
+    
+    /**
+     Logs out a user and discards their credentials
+     */
+    public func logout() {
+        cloudflare = nil
+        zones = nil
+        currentZone = nil
+    }
+    
+}
